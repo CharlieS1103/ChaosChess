7c8dd5614c1161f68685806e756f7a5a887d2eb4
CharlieS1103 <someonelikeswindows@gmail.com>
2022-03-17 17:13:10 -0400

    Style rules page

--- client/src/components/Rules.js
+++ client/src/components/Rules.js
@@ -7,26 +7,61 @@ export default class Rules extends React.Component {
             
         };
     }
+    componentDidMount() {
+        console.log("Component did mount")
+        document.body.style.backgroundColor = "#151E3F";
+        document.body.style.color = "#7FC29B";
+    }
     render() {
         return(
              <>
             <body>
-            <div className="rules-container">
-                <h1>Rules</h1>
-                <GravityRules className="gravity-container">
+                <Head>Rules</Head>
+            <RulesContainer className="rules-container">
+                
+                <RulesBox className="gravity-container">
                 <h2>Gravity Chess Rules</h2>
                 <p>
                 Gravity Chess is a chess variant that makes pieces fall towards their own sides
                 when an empty space is below them.
                 </p>
-                </GravityRules>
-            </div>
+                </RulesBox>
+                <RulesBox className="flipped-container">
+                <h2>Flipped Chess Rules</h2>
+                <p>
+                    Flipped Chess is a chess variant where the backrow and frontrow are swapped.
+                </p>
+                </RulesBox>
+            </RulesContainer>
             </body>
         </>)
 
     }
   
 } 
-const GravityRules = styled.div`
-    
+const Head = styled.h1`
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    font-size: 50px;
+    color: #f5f5f5;
+    background-color: #030027;
+    padding: 20px;
+    margin: 0px;
+`
+const RulesBox = styled.div`
+    margin: 10px;
+    padding: 10px;
+    border: solid 4px #7FC29B;
+    border-radius: 10px;
+    background-color: #030027;
+    color: #7FC29B;
+    text-align: center;
+
     `;
+const RulesContainer = styled.div`
+             display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+`;
