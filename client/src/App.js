ba9e106878398f3a19540d6fcfabc9fee20a9a12
CharlieS1103 <someonelikeswindows@gmail.com>
2022-03-17 17:35:28 -0400

    Add homepage styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,118 @@
 
+import React from "react";
 import styled from "styled-components";
-const chaosChess = () => {
+export default class App extends React.Component{
+  state = {
+    open: false,
+  };
 
-  const joinGravity = () => {
-      window.location.replace("/gravity")
+  handleButtonClick = () => {
+    this.setState((state) => {
+      return {
+        open: !state.open,
+      };
+    });
+  };
+  componentDidMount(){
+    document.body.style.backgroundColor = "#151E3F";
+    document.body.style.color = "#7FC29B";
   }
-  const joinFlipped = () => {
-      window.location.replace("/flipped")
-  }
-  const joinRules = () => {
-      window.location.replace("/rules")
-  }
-  const Button = styled.button`
-  background-color: black;
-  color: white;
-  font-size: 20px;
-  padding: 10px 60px;
-  border-radius: 5px;
-  margin: 10px 0px;
-  cursor: pointer;
-`;
+render(){
   return (
     <>
-      <h1>Chaos Chess</h1>
-      <Button onClick={() => {
-        joinGravity()
-      }}>Join Gravity Chess</Button>
-      <Button onClick={() => {
-        joinFlipped()
-      }}>Join Flipped Chess</Button>
-      <Button onClick={() => {
-        joinRules()
-      }}>Rules</Button>
+      <Head>Chaos Chess</Head>
+      <DropDownButton onClick={this.handleButtonClick}>
+        {this.state.open ? "Close" : "Open"}
+      </DropDownButton>
+      {this.state.open && (
+      <ButtonDropdown>
+        <StyledList>
+        <li>
+          <Button onClick={() => {
+            joinGravity()
+          }}>Join Gravity Chess</Button>
+          </li>
+        <li>    <Button onClick={() => {
+          joinFlipped()
+        }}>Join Flipped Chess</Button></li>
+  
+        <li>    <Button onClick={() => {
+          joinRules()
+        }}>Rules</Button></li>
+</StyledList>
+      </ButtonDropdown>
+      )}
     </>
    
   )
 }
+}
 
-export default chaosChess;
+const joinGravity = () => {
+  window.location.replace("/gravity")
+}
+const joinFlipped = () => {
+  window.location.replace("/flipped")
+}
+const joinRules = () => {
+  window.location.replace("/rules")
+}
+const Button = styled.button`
+    background-color: #7FC29B;
+    color: #151E3F;
+    border: solid 4px #151E3F;
+    font-size: 20px;
+    padding: 10px 60px;
+    border-radius: 10px;
+    margin: 10px 0px;
+    z-index: 2;
+    cursor: pointer;
+    border: solid 4px #7FC29B;
+
+`;
+const Head = styled.h1`
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  font-size: 50px;
+  color: #f5f5f5;
+  background-color: #030027;
+  padding: 20px;
+  margin: 0px;
+`;
+const ButtonDropdown = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+  padding: 10px;
+  border: solid 4px #7FC29B;
+  border-radius: 10px;
+  background-color: #030027;
+  color: #7FC29B;
+  text-align: center;
+  transition: all 0.5s ease;
+`;
+const DropDownButton = styled.button`
+  background-color: #7FC29B;
+  color: #151E3F;
+  border: solid 4px #151E3F;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-radius: 10px;
+  margin: 10px 0px;
+  z-index: 2;
+  cursor: pointer;
+  border: solid 4px #878896;
+  width: 100%;
+`;
+const StyledList = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
